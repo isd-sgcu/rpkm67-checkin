c2e5573f975c4b9dcfbe8def96f43e5ed78f8196
Gear <84141000+macgeargear@users.noreply.github.com>
2024-06-23 21:44:18 +0700

    feat: user

---
+++ internal/user/user.repository.go
@@ -0,0 +1,22 @@
+package user
+
+import (
+	"github.com/isd-sgcu/rpkm67-checkin/internal/model"
+	"gorm.io/gorm"
+)
+
+type Repository interface {
+	FindByEmail(email string, user *model.User) error
+}
+
+type repositoryImpl struct {
+	Db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &repositoryImpl{Db: db}
+}
+
+func (r *repositoryImpl) FindByEmail(email string, user *model.User) error {
+	return r.Db.First(user, "email = ?", email).Error
+}
