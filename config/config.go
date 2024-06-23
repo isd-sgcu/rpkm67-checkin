ac4973bbb9afc359eefc281d4af6d87c66273b4d
Gear <84141000+macgeargear@users.noreply.github.com>
2024-06-23 20:54:54 +0700

    feat: db config

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	Port string
+	Env  string
+}
+
+type DbConfig struct {
+	Url string
+}
+
+type Config struct {
+	App AppConfig
+	Db  DbConfig
+}
+
+func LoadConfig() (*Config, error) {
+	if os.Getenv("APP_ENV") == "" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	appConfig := AppConfig{
+		Port: os.Getenv("APP_PORT"),
+		Env:  os.Getenv("APP_ENV"),
+	}
+
+	dbConfig := DbConfig{
+		Url: os.Getenv("DB_URL"),
+	}
+
+	return &Config{
+		App: appConfig,
+		Db:  dbConfig,
+	}, nil
+}
+
+func (ac *AppConfig) IsDevelopment() bool {
+	return ac.Env == "development"
+}
