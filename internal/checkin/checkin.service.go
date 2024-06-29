db70129ac0e80a149f0c5902df0f983c7069eb44
JJnvn <raptor.jj05@gmail.com>
2024-06-29 23:58:12 +0700

    add error status

--- internal/checkin/checkin.service.go
+++ internal/checkin/checkin.service.go
@@ -2,6 +2,7 @@ package checkin
 
 import (
 	"context"
+	"errors"
 
 	proto "github.com/isd-sgcu/rpkm67-go-proto/rpkm67/checkin/checkin/v1"
 	"github.com/isd-sgcu/rpkm67-model/model"
@@ -9,6 +10,7 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
 )
 
 type Service interface {
@@ -32,9 +34,26 @@ func (s *serviceImpl) Create(_ context.Context, req *proto.CreateCheckInRequest)
 		UserID: req.UserId,
 	}
 
-	err := s.repo.Create(checkin)
+	var checkin_userIds []*model.CheckIn
+	err := s.repo.FindByUserId(req.UserId, &checkin_userIds)
 	if err != nil {
 		s.log.Named("Create").Error("Create: ", zap.Error(err))
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+	for _, v := range checkin_userIds {
+		if v.Event == req.Event && v.UserID == req.UserId {
+			return nil, status.Error(codes.AlreadyExists, "already checked in")
+		}
+	}
+	err = s.repo.Create(checkin)
+	if err != nil {
+		s.log.Named("Create").Error("Create: ", zap.Error(err))
+		if errors.Is(err, gorm.ErrInvalidDB) {
+			return nil, status.Error(codes.Internal, "database connection error")
+		}
+		if errors.Is(err, gorm.ErrInvalidData) {
+			return nil, status.Error(codes.InvalidArgument, "invalid data")
+		}
 		return nil, status.Error(codes.Internal, "internal error")
 	}
 
@@ -44,10 +63,30 @@ func (s *serviceImpl) Create(_ context.Context, req *proto.CreateCheckInRequest)
 }
 
 func (s *serviceImpl) FindByEmail(_ context.Context, req *proto.FindByEmailCheckInRequest) (*proto.FindByEmailCheckInResponse, error) {
+	if req.Email == "" {
+		s.log.Named("FindByUserEmail").Error("FindByUserEmail: invalid user ID")
+		return nil, status.Error(codes.InvalidArgument, "email cannot be empty")
+	}
+
 	var checkins []*model.CheckIn
-	if err := s.repo.FindByEmail(req.Email, &checkins); err != nil {
+	err := s.repo.FindByEmail(req.Email, &checkins)
+	if err != nil {
 		s.log.Named("FindByEmail").Error("FindByEmail: ", zap.Error(err))
+		if errors.Is(err, context.Canceled) {
+			return nil, status.Error(codes.Canceled, "request canceled by the client")
+		}
+		if errors.Is(err, gorm.ErrInvalidDB) {
+			return nil, status.Error(codes.Internal, "database connection error")
+		}
+		if errors.Is(err, context.DeadlineExceeded) {
+			return nil, status.Error(codes.DeadlineExceeded, "request timed out")
+		}
 		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	if len(checkins) == 0 {
+		s.log.Named("FindByUserEmail").Error("email not found")
+		return nil, status.Error(codes.NotFound, "email not found")
 	}
 
 	return &proto.FindByEmailCheckInResponse{
@@ -56,10 +95,30 @@ func (s *serviceImpl) FindByEmail(_ context.Context, req *proto.FindByEmailCheck
 }
 
 func (s *serviceImpl) FindByUserId(_ context.Context, req *proto.FindByUserIdCheckInRequest) (*proto.FindByUserIdCheckInResponse, error) {
+	if req.UserId == "" {
+		s.log.Named("FindByUserId").Error("FindByUserId: invalid user ID")
+		return nil, status.Error(codes.InvalidArgument, "user ID cannot be empty")
+	}
+
 	var checkins []*model.CheckIn
-	if err := s.repo.FindByUserId(req.UserId, &checkins); err != nil {
+	err := s.repo.FindByUserId(req.UserId, &checkins)
+	if err != nil {
 		s.log.Named("FindByUserId").Error("FindByUserId: ", zap.Error(err))
+		if errors.Is(err, context.Canceled) {
+			return nil, status.Error(codes.Canceled, "request canceled by the client")
+		}
+		if errors.Is(err, gorm.ErrInvalidDB) {
+			return nil, status.Error(codes.Internal, "database connection error")
+		}
+		if errors.Is(err, context.DeadlineExceeded) {
+			return nil, status.Error(codes.DeadlineExceeded, "request timed out")
+		}
 		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	if len(checkins) == 0 {
+		s.log.Named("FindByUserId").Error("user not found")
+		return nil, status.Error(codes.NotFound, "user not found")
 	}
 
 	return &proto.FindByUserIdCheckInResponse{
