d660bf895681276322b5093091c00f886d3b7a49
Gear <84141000+macgeargear@users.noreply.github.com>
2024-07-11 14:38:18 +0700

    feat: checkin tracer

--- internal/checkin/checkin.repository.go
+++ internal/checkin/checkin.repository.go
@@ -1,32 +1,45 @@
 package checkin
 
 import (
+	"context"
+
 	"github.com/isd-sgcu/rpkm67-model/model"
+	"go.opentelemetry.io/otel/trace"
 	"gorm.io/gorm"
 )
 
 type Repository interface {
-	Create(checkIn *model.CheckIn) error
-	FindByEmail(email string, checkIns *[]*model.CheckIn) error
-	FindByUserId(userId string, checkIns *[]*model.CheckIn) error
+	Create(ctx context.Context, checkIn *model.CheckIn) error
+	FindByEmail(ctx context.Context, email string, checkIns *[]*model.CheckIn) error
+	FindByUserId(ctx context.Context, userId string, checkIns *[]*model.CheckIn) error
 }
 
 type repositoryImpl struct {
-	Db *gorm.DB
+	Db     *gorm.DB
+	tracer trace.Tracer
 }
 
-func NewRepository(db *gorm.DB) Repository {
-	return &repositoryImpl{Db: db}
+func NewRepository(db *gorm.DB, tracer trace.Tracer) Repository {
+	return &repositoryImpl{Db: db, tracer: tracer}
 }
 
-func (r *repositoryImpl) Create(checkIn *model.CheckIn) error {
+func (r *repositoryImpl) Create(ctx context.Context, checkIn *model.CheckIn) error {
+	_, span := r.tracer.Start(ctx, "repository.checkin.Create")
+	defer span.End()
+
 	return r.Db.Create(checkIn).Error
 }
 
-func (r *repositoryImpl) FindByEmail(email string, checkIns *[]*model.CheckIn) error {
+func (r *repositoryImpl) FindByEmail(ctx context.Context, email string, checkIns *[]*model.CheckIn) error {
+	_, span := r.tracer.Start(ctx, "repository.checkin.FindByEmail")
+	defer span.End()
+
 	return r.Db.Where("email = ?", email).Find(&checkIns).Error
 }
 
-func (r *repositoryImpl) FindByUserId(userId string, checkIns *[]*model.CheckIn) error {
+func (r *repositoryImpl) FindByUserId(ctx context.Context, userId string, checkIns *[]*model.CheckIn) error {
+	_, span := r.tracer.Start(ctx, "repository.checkin.FindByUserId")
+	defer span.End()
+
 	return r.Db.Where("user_id = ?", userId).Find(&checkIns).Error
 }
--- internal/checkin/checkin.service.go
+++ internal/checkin/checkin.service.go
@@ -7,7 +7,8 @@ import (
 	"github.com/isd-sgcu/rpkm67-checkin/constant"
 	proto "github.com/isd-sgcu/rpkm67-go-proto/rpkm67/checkin/checkin/v1"
 	"github.com/isd-sgcu/rpkm67-model/model"
-
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -20,15 +21,23 @@ type Service interface {
 
 type serviceImpl struct {
 	proto.UnimplementedCheckInServiceServer
-	repo Repository
-	log  *zap.Logger
+	repo   Repository
+	log    *zap.Logger
+	tracer trace.Tracer
 }
 
-func NewService(repo Repository, log *zap.Logger) Service {
-	return &serviceImpl{repo: repo, log: log}
+func NewService(repo Repository, log *zap.Logger, tracer trace.Tracer) Service {
+	return &serviceImpl{repo: repo, log: log, tracer: tracer}
 }
 
-func (s *serviceImpl) Create(_ context.Context, req *proto.CreateCheckInRequest) (*proto.CreateCheckInResponse, error) {
+func (s *serviceImpl) Create(ctx context.Context, req *proto.CreateCheckInRequest) (*proto.CreateCheckInResponse, error) {
+	span := trace.SpanFromContext(ctx)
+	span.SetAttributes(
+		attribute.String("req.user.id", req.UserId),
+		attribute.String("req.user.email", req.Email),
+		attribute.String("req.event", req.Event),
+	)
+
 	checkin := &model.CheckIn{
 		Email:  req.Email,
 		Event:  req.Event,
@@ -36,7 +45,7 @@ func (s *serviceImpl) Create(_ context.Context, req *proto.CreateCheckInRequest)
 	}
 
 	var checkin_userIds []*model.CheckIn
-	err := s.repo.FindByUserId(req.UserId, &checkin_userIds)
+	err := s.repo.FindByUserId(ctx, req.UserId, &checkin_userIds)
 	if err != nil {
 		s.log.Named("Create").Error("Create: ", zap.Error(err))
 		return nil, status.Error(codes.Internal, constant.InternalServerErrorMessage)
@@ -48,7 +57,9 @@ func (s *serviceImpl) Create(_ context.Context, req *proto.CreateCheckInRequest)
 			return nil, status.Error(codes.AlreadyExists, constant.AlreadyCheckinErrorMessage)
 		}
 	}
-	err = s.repo.Create(checkin)
+	span.AddEvent("Verify user checkin not duplicate")
+
+	err = s.repo.Create(ctx, checkin)
 	if err != nil {
 		s.log.Named("Create").Error("Create: ", zap.Error(err))
 		if status.Code(err) == codes.AlreadyExists {
@@ -62,20 +73,28 @@ func (s *serviceImpl) Create(_ context.Context, req *proto.CreateCheckInRequest)
 		}
 		return nil, status.Error(codes.Internal, constant.InternalServerErrorMessage)
 	}
+	span.AddEvent("Checkin created")
 
 	return &proto.CreateCheckInResponse{
 		CheckIn: ModelToProto(checkin),
 	}, nil
 }
 
-func (s *serviceImpl) FindByEmail(_ context.Context, req *proto.FindByEmailCheckInRequest) (*proto.FindByEmailCheckInResponse, error) {
+func (s *serviceImpl) FindByEmail(ctx context.Context, req *proto.FindByEmailCheckInRequest) (*proto.FindByEmailCheckInResponse, error) {
+	span := trace.SpanFromContext(ctx)
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.String("req.user.email", req.Email),
+	)
+
 	if req.Email == "" {
 		s.log.Named("FindByUserEmail").Error("FindByUserEmail: invalid user Email")
 		return nil, status.Error(codes.InvalidArgument, constant.ArgumentEmptyErrorMessage)
 	}
 
 	var checkins []*model.CheckIn
-	err := s.repo.FindByEmail(req.Email, &checkins)
+	err := s.repo.FindByEmail(ctx, req.Email, &checkins)
 	if err != nil {
 		s.log.Named("FindByEmail").Error("FindByEmail: ", zap.Error(err))
 		if status.Code(err) == codes.Canceled {
@@ -86,22 +105,31 @@ func (s *serviceImpl) FindByEmail(_ context.Context, req *proto.FindByEmailCheck
 		}
 		return nil, status.Error(codes.Internal, constant.InternalServerErrorMessage)
 	}
+	span.AddEvent("Checkin found")
 
 	return &proto.FindByEmailCheckInResponse{
 		CheckIns: ModelToProtoList(checkins),
 	}, nil
 }
 
-func (s *serviceImpl) FindByUserId(_ context.Context, req *proto.FindByUserIdCheckInRequest) (*proto.FindByUserIdCheckInResponse, error) {
+func (s *serviceImpl) FindByUserId(ctx context.Context, req *proto.FindByUserIdCheckInRequest) (*proto.FindByUserIdCheckInResponse, error) {
+	ctx, span := s.tracer.Start(ctx, "service.checkin.FindByUserId")
+	span.SetAttributes(
+		attribute.String("req.user.id", req.UserId),
+	)
+	defer span.End()
+
 	if req.UserId == "" {
 		s.log.Named("FindByUserId").Error("FindByUserId: invalid user ID")
+		span.RecordError(status.Error(codes.InvalidArgument, constant.ArgumentEmptyErrorMessage))
 		return nil, status.Error(codes.InvalidArgument, constant.ArgumentEmptyErrorMessage)
 	}
 
 	var checkins []*model.CheckIn
-	err := s.repo.FindByUserId(req.UserId, &checkins)
+	err := s.repo.FindByUserId(ctx, req.UserId, &checkins)
 	if err != nil {
 		s.log.Named("FindByUserId").Error("FindByUserId: ", zap.Error(err))
+		span.RecordError(status.Error(codes.InvalidArgument, constant.ArgumentEmptyErrorMessage))
 
 		if status.Code(err) == codes.Canceled {
 			return nil, status.Error(codes.Canceled, constant.RequestCancelledErrorMessage)
